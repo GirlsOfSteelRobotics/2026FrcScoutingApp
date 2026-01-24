b193c0239c0d37932e586cdf8783ede3e5dc3619
Jeannine Zhou <jz@Jeannines-MacBook-Air.local>
2026-01-24 12:25:28 -0500

    Bunch of graphs of fuel in hub

--- data_container.py
+++ data_container.py
@@ -8,7 +8,13 @@ EVENT_CODE = "mock_data"
 
 def load_scouted_data():
     scouted_data = pd.read_csv(script_directory / f'data/{EVENT_CODE}/scouted_data.csv')
+    scouted_data[['Team Number', 'Match Number']] = scouted_data[['Team Number', 'Match Number']].astype("string")
 
     return scouted_data
+
+def get_Teams_in_Match():
+    Teams_in_Match = ["118", "67", "8393", "3504", "254", "1678"]
+
+    return (Teams_in_Match)
 
 
