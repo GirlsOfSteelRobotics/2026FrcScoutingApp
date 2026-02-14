f5cbb0bb6e50c9861be2f21ec5fcc11c4f32c229
ooshcoding <aashimbhatt@gmail.com>
2026-02-14 12:19:16 -0500

    download statbotics data

--- download_external_data.py
+++ download_external_data.py
@@ -1,6 +1,13 @@
 import pathlib
 from metadata import CURRENT_EVENT
-from utils.tba_utils import download_tba_event_matches
+
+from utils.statbotics_utils import (
+    download_statbotics_matches,
+    download_statbotics_event_teams,
+)
+
+from utils.tba_utils import download_tba_event_matches, download_tba_event_teams
+
 
 def download_external_data(event):
     """
@@ -11,8 +18,13 @@ def download_external_data(event):
     data_directory = script_directory / "data" / event
     data_directory.mkdir(parents=True, exist_ok=True)
 
-    #
+    # Download TBA data
     download_tba_event_matches(event, data_directory / "tba_matches.json")
+    download_tba_event_teams(event, data_directory / "tba_teams.json")
+
+    # Download Statbotics data
+    download_statbotics_matches(event, data_directory / "statbotics_matches.json")
+    download_statbotics_event_teams(event, data_directory / "statbotics_teams.json")
 
 
 if __name__ == "__main__":
