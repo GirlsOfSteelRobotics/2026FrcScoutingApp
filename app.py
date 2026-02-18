f65496b691d7ecad4d1e8bdbb59eee6ac0f474bc
Jeannine Zhou <jz@Jeannines-MacBook-Air.local>
2026-02-17 20:13:00 -0500

    started app stuff

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 from shiny import App, ui
+import pandas as pd
+import numpy as np
+import plotly.express as px
+from shiny import reactive, render, module
+from shiny import App, ui
+from shinywidgets import output_widget, render_widget
+from data_container import load_pit_data, load_scouted_data
 
 
 app_ui = ui.page_navbar(
