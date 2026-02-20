258be296f315b87735f90205ec3d9c64cde1d681
GosProgrammers <girlsofsteelrobotics@gmail.com>
2026-02-19 20:39:18 -0500

    transfer to shiny app

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 from shiny import App, ui
 
+from overview_tab import overview_tab_server, overview_tab_ui
 
 app_ui = ui.page_navbar(
-    ui.nav_panel("A", "Page A content"),
-    ui.nav_panel("B", "Page B content"),
-    ui.nav_panel("C", "Page C content"),
-    title="App with navbar",
+    ui.nav_panel("Overview", overview_tab_ui("overview")),
+    # ui.nav_panel("Matches", "matches"),
+    title="2026FRCScoutingApp",
 )
 
 
 def server(input, output, session):
     pass
+    overview_tab_server("overview")
+    # general_match_server("matches")
 
 
 app = App(app_ui, server)
