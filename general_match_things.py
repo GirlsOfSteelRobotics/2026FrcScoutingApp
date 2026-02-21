061a61f1b9ecd140d46a56e7ef9033ea253bced5
Jeannine Zhou <jz@Jeannines-MacBook-Air.local>
2026-02-21 13:54:04 -0500

    transfer auto graphs to shiny

--- general_match_things.py
+++ general_match_things.py
@@ -16,12 +16,18 @@ def general_match_ui():
             ui.sidebar(
                 ui.output_ui("match_list_combobox"),
             ),
-            ui.navset_tab(
-                ui.nav_panel("Teleop",
-                ui.card(output_widget("teleop_fuel_in_hub")),
+                ui.navset_tab(
+                    ui.nav_panel("Match List",
+                                 ui.card(output_widget("auto_fuel_in_hub")),
+                                        ui.card(output_widget("auto_climbing_frequency")),
+                ),
+                ui.navset_tab(
+                    ui.nav_panel("Teleop",
+                                 ui.card(output_widget("teleop_fuel_in_hub")),
                     ui.card(output_widget("teleop_fuel_passed_total")),
                     ui.card(output_widget("teleop_fuel_passed_avg")),
                 ),
+                )
             )
         )
     )
@@ -70,5 +76,26 @@ def general_match_server(input, output, session):
                      color_discrete_sequence=custom_colors)
         return fig
 
+    @render_widget
+    def auto_fuel_in_hub():
+        new_df = get_teams_in_match()
+        avg_team = new_df.groupby("Team Number").mean(numeric_only=True)
+        custom_colors = ["#194f55", "#54808e", "#243454"]
+        fig = px.bar(avg_team, y="Auto Fuel", title="Fuel in Hub (Auto) per Robot",
+                     color_discrete_sequence=custom_colors)
+        return fig
+
+    @render_widget
+    def auto_climbing_frequency():
+        auto_climbing_status_df = scouted_data.groupby("Team Number")["Auto Climbing Status"].value_counts().unstack(
+            fill_value=0).reset_index()
+        auto_climbing_status_df["Climb Freq"] = auto_climbing_status_df[True] / (
+                auto_climbing_status_df[True] + auto_climbing_status_df[False])
+        auto_climbing_status_df["No Climb Freq"] = auto_climbing_status_df[False] / (
+                auto_climbing_status_df[True] + auto_climbing_status_df[False])
+        custom_colors = ["#194f55", "#54808e", "#243454"]
+        fig = px.bar(auto_climbing_status_df, x="Team Number", y=["Climb Freq", "No Climb Freq"],
+                     title="Auto Climbing Frequency", color_discrete_sequence=custom_colors)
+        return fig
 
 #app = App(general_match_ui("match"), lambda input, output, session: general_match_server("match", input, output, session))
